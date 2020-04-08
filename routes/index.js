203004e8d71be7fbd196a08466e64ee98177f69e
khyoo1221 <khyoo1221gmail.com>
2020-04-08 17:03:46 +0900

    cors 적용

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET page. */
 router.get('/', function(req, res, next) {
-  console.log("get");
-  console.log(req.query.url);
+  console.log("get");  
 
-  res.render('index', { url: req.query.url });
+  var temp_geoJson = '/data/info.geojson';
+
+  res.render('index', { geourl: temp_geoJson });
+});
+
+router.get('/geourl', function(req, res, next) {
+  console.log("get");  
+
+  res.render('index', { geourl: req.query.url });
 });
 
 
